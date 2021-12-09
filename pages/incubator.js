787fe20edf191b1d0cc8b02a56fd167f728f3f06
pextech <mcstain1639@gmail.com>
2021-12-09 11:21:49 +0200

    fix deployment

--- pages/incubator.js
+++ pages/incubator.js
@@ -19,7 +19,7 @@ import SidebarTwo from '../components/SidebarTwo';
 import links from '../contexts/utils/links';
 import ComingSoon from '../components/ComingSoon';
 import { useDetectOutsideClick } from '../components/UseDetectOutsideClick';
-import LoginPage from '../components/consultancy/helperFiles/login-page';
+import LoginPage from '../components/Consultancy/helperFiles/LoginPage';
 
 const IncubatorPage = function () {
   const [startups, setStartups] = useState([]);
