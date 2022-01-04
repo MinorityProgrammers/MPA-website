9f16b661b24b6cbb280a6c2783f0fff97ce6e409
M-Rb3 <rabea9220@gmail.com>
2022-01-04 02:34:59 +0200

    update userInfo in localstorage

--- contexts/actions/profile/updateProfileJSON.js
+++ contexts/actions/profile/updateProfileJSON.js
@@ -1,13 +1,13 @@
-import axios from 'axios';
-import jwt from 'jsonwebtoken';
-import axiosInstance from '../../../helpers/axiosInstance';
+import axios from "axios";
+import jwt from "jsonwebtoken";
+import axiosInstance from "../../../helpers/axiosInstance";
 
 import {
   UPDATE_PROFILE_LOADING,
   UPDATE_PROFILE_SUCCESS,
   UPDATE_PROFILE_ERROR,
-} from '../actionTypes';
-import { successToast, errorToast } from '../../utils/toasts';
+} from "../actionTypes";
+import { successToast, errorToast } from "../../utils/toasts";
 
 export const updateProfileJSON = (id, body, token) => (dispatch) => {
   dispatch({
@@ -15,31 +15,37 @@ export const updateProfileJSON = (id, body, token) => (dispatch) => {
   });
 
   // axios post request
-  const token = window.localStorage.getItem('jwtToken');
+  const token = window.localStorage.getItem("jwtToken");
 
   axios
     .patch(`${process.env.BASE_URI}/user/updateProfile/${id}`, body, {
       headers: {
-        'Content-type': 'application/json',
+        "Content-type": "application/json",
         Authorization: `Bearer ${token}`,
       },
     })
     .then((res) => {
       // setting updated user
 
-      localStorage.setItem('userInfo', JSON.stringify(res.data.data));
+      localStorage.setItem(
+        "userInfo",
+        JSON.stringify({
+          uesr: res.data.data,
+          token: token,
+        })
+      );
       dispatch({
         type: UPDATE_PROFILE_SUCCESS,
         payload: res.data,
       });
-      successToast('successfully updated profile');
+      successToast("successfully updated profile");
     })
     .catch((err) => {
       console.log(err.response.data);
-      errorToast('failed to update profile');
+      errorToast("failed to update profile");
       dispatch({
         type: UPDATE_PROFILE_ERROR,
-        payload: err.response ? err.response.data : 'COULD NOT CONNECT',
+        payload: err.response ? err.response.data : "COULD NOT CONNECT",
       });
     });
 };
