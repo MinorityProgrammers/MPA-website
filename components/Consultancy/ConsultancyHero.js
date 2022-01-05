66dc77e9165d1723624608969dbfeb55fc502922
Kevin Hernandez <kevin.hernandez1802@gmail.com>
2022-01-04 18:30:50 -0600

    redesign of banners

--- components/Consultancy/ConsultancyHero.js
+++ components/Consultancy/ConsultancyHero.js
@@ -11,6 +11,11 @@ const ConsultancyHero = function () {
                 Consultancy
               </h1>
               <p className="consultancyHero-txt">Get your idea turned into a complete web application ready for market with our transparent task-based escrow system that gives you as client, the control over how your product is being built.  </p>
+              <div className="incubator__button">
+                <div  className="button btn-gradient">
+                  Get Quote
+                </div>
+              </div>
             </div>
             <div>
               <img src = 'assets/images/consultancy/consultancyHero.png'/>
