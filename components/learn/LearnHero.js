fbc5b618e8cceb01e3722acf7655973c4a28b5e2
pextech <mcstain1639@gmail.com>
2022-01-06 15:09:45 +0200

    banners refactor

--- components/learn/LearnHero.js
+++ components/learn/LearnHero.js
@@ -1,6 +1,9 @@
 import React from 'react';
 
 const LearnHero = function () {
+  function scrollToModule() {
+    window.scrollTo(400, 400);
+  }
   return (
     <section className="incubator-wrapper">
       <div className="mentorshipP-header">
@@ -12,13 +15,13 @@ const LearnHero = function () {
               </h1>
               <p className="consultancyHero-txt">Learn the hottest skills in blockchain, earn NFT credentials that recruiters from the top technology companies will see through our decentralized talent sourcing system.</p>
               <div className="incubator__button">
-                <div  className="button btn-gradient">
-                  Enroll for Free 
+                <div className="button btn-gradient" onClick={() => { scrollToModule(); }}>
+                  Enroll for Free
                 </div>
               </div>
             </div>
             <div>
-              <img src = 'assets/images/learn-page/learnHero.png'/>
+              <img src="assets/images/learn-page/learnHero.png" alt="" />
             </div>
           </div>
         </div>
