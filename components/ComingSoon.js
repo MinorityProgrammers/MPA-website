b297921cd30f3e954951bb7cdaf26d4b33bd5496
Ali Alrubaye <alialrubaye1990@hotmail.com>
2022-01-09 17:50:39 -0500

    Added 6 seconds delay to coming soon modal

--- components/ComingSoon.js
+++ components/ComingSoon.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const ComingSoon = ({ closeClick }) => (
-  <div className="comingsoon-container">
-    <div className="CSimg-container">
-      <img
-        src="/assets/images/underConstruction.png"
-        alt="profile"
-        className="CS-img"
-      />
+const ComingSoon = ({ closeClick }) => {
+  const [delay, setDelay] = useState(true);
+  useEffect(() => {
+    setTimeout(() => {
+      setDelay(false);
+    }, 6000);
+  }, [1]);
+
+  if (delay) {
+    return null;
+  }
+
+  return (
+    <div>
+      <div className="comingsoon-container">
+        <div className="CSimg-container">
+          <img
+            src="/assets/images/underConstruction.png"
+            alt="profile"
+            className="CS-img"
+          />
+        </div>
+        <p className="CS-text">Coming soon...</p>
+        <div className="dropdown-login-button">
+          <button
+            className="btn tw-bg-blue-700 tw-w-44 tw-text-white hover:tw-text-white"
+            onClick={closeClick}
+          >
+            Close
+          </button>
+        </div>
+      </div>
     </div>
-    <p className="CS-text">Coming soon...</p>
-    <div className="dropdown-login-button">
-      <button
-        className="btn tw-bg-blue-700 tw-w-44 tw-text-white hover:tw-text-white"
-        onClick={closeClick}
-      >
-        Close
-      </button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default ComingSoon;
