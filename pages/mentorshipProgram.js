dd787136f412679ea149c183d678bef4a2775f4f
M-Rb3 <rabea9220@gmail.com>
2022-01-19 14:31:21 +0200

    unregistered users issue

--- pages/mentorshipProgram.js
+++ pages/mentorshipProgram.js
@@ -26,11 +26,14 @@ const mentorshipProgram = () => {
   }
 
   useEffect(() => {
-    const userInfo = JSON.parse(window.localStorage.getItem('userInfo')).user;
-    if (userInfo.has_mentorship) {
-      window.location.href = userInfo.is_mentor
-        ? 'mentorship/mentor'
-        : 'mentorship/mentee';
+    let userInfo = JSON.parse(window.localStorage.getItem('userInfo'));
+    if (userInfo) {
+      userInfo = userInfo.user;
+      if (userInfo.has_mentorship) {
+        window.location.href = userInfo.is_mentor
+          ? 'mentorship/mentor'
+          : 'mentorship/mentee';
+      }
     }
     if (data === null) {
       setActive(false);
@@ -39,23 +42,23 @@ const mentorshipProgram = () => {
     }
   }, []);
   return (
-    <Layout pageTitle="MPA - Mentorship SignUp">
+    <Layout pageTitle='MPA - Mentorship SignUp'>
       <HomepageNav
         open={open}
         setOpen={setOpen}
         setData={setData}
-        page="MentorshipProgram"
+        page='MentorshipProgram'
       />
       <SidebarTwo
         open={open}
         setOpen={setOpen}
         links={links}
-        active="Home"
+        active='Home'
         handleClick={handleClick}
       />
       {hide === false && <ComingSoon closeClick={handleClick} />}
       <MentorshipProgramHero />
-      <section className="section__mentorshipProgram">
+      <section className='section__mentorshipProgram'>
         <MentorshipProgramHome
           data={data}
           setClickRegister={setClickRegister}
