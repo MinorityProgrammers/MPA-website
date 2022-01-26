839ebcf40b9cebec0e72ead137fe40569f2192ac
M-Rb3 <rabea9220@gmail.com>
2022-01-26 18:31:50 +0200

    about-us responsiveness

--- components/homepage/HomePageAboutUs.js
+++ components/homepage/HomePageAboutUs.js
@@ -5,28 +5,30 @@ import 'react-lite-youtube-embed/dist/LiteYouTubeEmbed.css';
 function HomePageAboutUs() {
   return (
     <section className="homepage__about">
-      <div className="container tw-relative tw-justify-between tw-flex tw-flex-row tw-w-full tw-h-full lg:tw-flex-col-reverse">
-        <div className="tw-h-full tw-min-w-full video-container tw-flex-col">
-          <LiteYouTubeEmbed
-            playerClass="yt-playbtn"
-            activeClass="lyt-activated"
-            wrapperClass="yt-lite"
-            id="VZmd8EOj3UA"
-            title="minority programmers introduction video"
-          />
-        </div>
-        <div className="homepage__about-right tw-flex-col">
-          <h2 className="about-us__title top__part__title">
-            About Us
-            <img
-              src="/assets/images/home-page/about-title-icon.svg"
-              alt="blockchain"
+      <div className="container tw-relative tw-w-full tw-h-full">
+        <div className="row tw-justify-between">
+          <div className="tw-h-full col-lg-8  video-container tw-flex-col">
+            <LiteYouTubeEmbed
+              playerClass="yt-playbtn"
+              activeClass="lyt-activated"
+              wrapperClass="yt-lite"
+              id="VZmd8EOj3UA"
+              title="minority programmers introduction video"
             />
-          </h2>
-          <p className="about-us__text tw-text-xl tw-text-white">
-            We are an international network of developers unifying together to
-            build socially impactful projects & spread STEM education to marginalized communitie
-          </p>
+          </div>
+          <div className="homepage__about-right col-lg-4 tw-flex-col">
+            <h2 className="about-us__title top__part__title">
+              About Us
+              <img
+                src="/assets/images/home-page/about-title-icon.svg"
+                alt="blockchain"
+              />
+            </h2>
+            <p className="about-us__text tw-text-xl tw-text-white">
+              We are an international network of developers unifying together to
+              build socially impactful projects & spread STEM education to marginalized communitie
+            </p>
+          </div>
         </div>
       </div>
     </section>
