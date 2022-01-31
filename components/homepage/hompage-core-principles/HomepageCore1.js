117601bdaf8dd94295f691f243123bee31e73b07
Jiku Godwill Nsanwi <65053264+Jikugodwill@users.noreply.github.com>
2022-01-31 01:12:22 +0100

    Added classes and added dynamic principle production based on content

--- components/homepage/hompage-core-principles/HomepageCore1.js
+++ components/homepage/hompage-core-principles/HomepageCore1.js
@@ -2,13 +2,32 @@ import React from 'react';
 import PrincipleCard from './PrincipleCard';
 
 const HomepageCore1 = () => {
+  const principlesList = [
+    {
+      id: 1,
+      image: "",
+      title: "Culture",
+      content: "We encourage inclusivity and motivate our students and employees to think outside the box, become excellent collaborators"
+    }, {
+      id: 2,
+      image: "",
+      title: "Code",
+      content:"With our professional team driven experience, we focus on shipping industry grade products that solves real world problems"
+    },
+    {
+      id: 3,
+      image:"",
+      title: "Community",
+      content:"We strive to build a supportive community for developers to engage, share knowledge, motivate and grow into valuable assets."
+    }
+  ]
   return (
-    <section className='tw-container tw-mx-auto tw-flex md:tw-flex-col tw-flex-col tw-gap-y-10 tw-justify-center tw-bg-[#14152A] tw-items-center tw-content-center'>
+    <section className='core-principles tw-flex md:tw-flex-col tw-flex-col tw-justify-center tw-items-center tw-content-center tw-px-8'>
       <h2 className='top__part__title'>Our Core Principles</h2>
-      <section className='tw-container tw-mx-auto tw-flex md:tw-flex-col tw-flex-row tw-justify-center tw-items-center tw-h-screen tw-gap-6 md:tw-gap-x-6 md:tw-gap-y-4'>
-        <PrincipleCard />
-        <PrincipleCard />
-        <PrincipleCard />
+      <section className='tw-container tw-mx-auto tw-flex md:tw-flex-col tw-flex-row tw-justify-center tw-items-center tw-h-screen md:tw-h-11/12 md:tw-py-8 tw-gap-6 md:tw-gap-x-6 md:tw-gap-y-4'>
+        {principlesList.map((principle)=>{
+          return <PrincipleCard key={principle.id} title={principle.title} image={principle.image} content={principle.content} />
+        })}
       </section>
     </section>
   );
--- components/homepage/hompage-core-principles/PrincipleCard.js
+++ components/homepage/hompage-core-principles/PrincipleCard.js
@@ -1,15 +1,15 @@
 import React from 'react';
 // import handShake from "../../../public/assets/images/home-page/principles/handshake.png";
 
-const PrincipleCard = () => {
+const PrincipleCard = (props) => {
     return (
-        <div className='tw-flex-[1_1_30%] tw-flex tw-rounded-3xl tw-border-2 tw-border-[#804D2] tw-justify-evenly tw-items-center tw-content-evenly tw-px-6 tw-py-8'>
-            <div className='tw-rounded-full tw-border-4 tw-w-10 tw-h-10 tw-p-4 tw-border-[#804D2] tw-shadow-[0px_0px_3px_7px_#3E385D] '>
-                <img src="" alt="principle icon"/>
+        <div className='tw-flex-[1_1_30%] tw-flex tw-rounded-3xl tw-border-2 tw-border-[#804D2] tw-justify-evenly tw-items-center tw-flex-col tw-content-evenly tw-gap-6 tw-px-6 tw-py-8 principle-card'>
+            <div className='tw-rounded-full tw-border-1 tw-w-8 tw-h-8 tw-p-9 tw-border-[#804D2] principle-card__elipse-shadow '>
+                <img src={props.image} alt="principle icon"/>
             </div>
             <div className='tw-flex tw-container tw-mx-auto tw-flex-col tw-justify-evenly tw-content-evenly tw-items-center tw-gap-6'>
-                <h4>Culture</h4>
-                <p>We encourage inclusivity and motivate our students and employees to think outside the box, become excellent collaborators</p>
+                <h4 className='principle-card__heading'>{props.title}</h4>
+                <p className="principle-card__main-text">{props.content}</p>
             </div>
         </div>
     );
